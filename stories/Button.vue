f72da38236c49353d2cabf3e07e02a5a2160fbb5
Thomas Aull <post@thomasaull.de>
2021-03-27 16:15:30 +0100

    Remove unneeded stuff

--- stories/Button.vue
+++ stories/Button.vue
@@ -1,60 +1,14 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <button>
+    Label
+  </button>
 </template>
 
 <script>
-import './button.css';
-
 import { testFunction } from './test'
+testFunction()
 
 export default {
   name: 'my-button',
-
-  props: {
-    label: {
-      type: String,
-      required: true,
-    },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: String,
-      default: 'medium',
-      validator: function (value) {
-        return ['small', 'medium', 'large'].indexOf(value) !== -1;
-      },
-    },
-    backgroundColor: {
-      type: String,
-    },
-  },
-
-  computed: {
-    classes() {
-      return {
-        'storybook-button': true,
-        'storybook-button--primary': this.primary,
-        'storybook-button--secondary': !this.primary,
-        [`storybook-button--${this.size}`]: true,
-      };
-    },
-    style() {
-      return {
-        backgroundColor: this.backgroundColor,
-      };
-    },
-  },
-
-  mounted() {
-    testFunction()
-  },
-
-  methods: {
-    onClick() {
-      this.$emit('onClick');
-    },
-  },
 };
 </script>
